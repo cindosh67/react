240aaed4c740a7053602133a5c6f1c44e987d705
cindy <r.cindy67@gmail.com>
2022-12-14 08:34:23 +0100

    ajout favicon

--- src/App.js
+++ src/App.js
@@ -3,20 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Error from "./pages/NotFound";
-// import Lodging from "./pages/Lodging";
+import About from "./pages/About";
+import Error from "./pages/NotFound";
+import Lodging from "./pages/Lodging";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Home /> */}
       <Routes>
         <Route path="/*" element={<Home />} />
-        {/* <Route path="/About" element={<About />} />
+        <Route path="/About" element={<About />} />
         <Route path="/lodging/:id" element={<Lodging />} />
-        <Route path="/*" element={<Error />} /> */}
+        <Route path="/*" element={<Error />} />
       </Routes>
       <Footer />
     </div>
